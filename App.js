567e842581c4c5e294aefa7e847716772d2377f6
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2023-11-09 17:47:44 -0300

    rode um dia

--- App.js
+++ App.js
@@ -9,21 +9,15 @@ import Login from '../autenticacao/src/components/Login.js';
 // import Home from 'react-native-firebase/src/components/Home.js';
 
 
-
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Home" component={Home} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
